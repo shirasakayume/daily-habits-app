5c6bcc19403ba97da75e35db42aba6d9331eb11c
shirasakayume <kana.jj.ru.jakarta@gmail.com>
2026-09-18 18:29:56 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ const APP_FILES = [
   "./style.css",
   "./script.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  ".icon-192.png",
+  ".icon-512.png"
 ];
 
 self.addEventListener("install", event => {
